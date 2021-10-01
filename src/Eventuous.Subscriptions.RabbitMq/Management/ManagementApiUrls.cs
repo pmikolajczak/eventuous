63bc98bd63b83fe2efb409e5b2684f6e83de3e89
ABAX/patryk.mikolajczak <patryk.mikolajczak@abax.com>
2021-10-01 12:19:38 +0200

    small steps

---
+++ src/Eventuous.Subscriptions.RabbitMq/Management/ManagementApiUrls.cs
@@ -0,0 +1,7 @@
+﻿namespace Eventuous.Subscriptions.RabbitMq.Management {
+    internal static class ManagementApiUrls {
+        
+        // GET /api/queues/{vhost}/{qname}
+        public const string QueueMetricsUrlFormat = "/api/queues/{0}/{1}";
+    }
+}
---
+++ src/Eventuous.Subscriptions.RabbitMq/Management/RabbitMqManagementApiClient.cs
@@ -0,0 +1,12 @@
+﻿namespace Eventuous.Subscriptions.RabbitMq.Management {
+    using System.Net.Http;
+    using RabbitMQ.Client;
+
+    public class RabbitMqManagementApiClient {
+        private readonly HttpClient _httpClient;
+
+        public RabbitMqManagementApiClient(ConnectionFactory connectionFactory) {
+            _httpClient = new HttpClient();
+        }
+    }
+}
